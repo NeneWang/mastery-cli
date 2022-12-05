cfd1511d43d10972a893f4f4f66ef0965421d51e
NelsonWang <wangnelson2@gmail.com>
2022-12-05 09:55:45 -0500

    Test dev

---
+++ test.js
@@ -0,0 +1,15 @@
+// const alert = require('./index')
+
+const utils = require('./utils/utils');
+
+const constants = require('./utils/constants');
+const { getMaidHeader, getTalk } = utils;
+
+
+// 
+
+message()
+
+console.log(`${getMaidHeader()} ${chalk(message)}`);
+
+
