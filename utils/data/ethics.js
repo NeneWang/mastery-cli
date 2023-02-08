a21bbf2e95332b7ee1cd416deff18ba8e945b61b
NelsonWang <wangnelson2@gmail.com>
2023-02-08 17:18:47 -0500

    ethics | prag programming :crown: :mage_woman:

---
+++ utils/data/ethics.js
@@ -0,0 +1,20 @@
+
+// https://github.com/HugoMatilla/The-Pragmatic-Programmer
+/**
+ * I would suggest going to the Quick reference section and just transform the tips (after reading the book.).
+ */
+
+const pragmatic_programmer = [
+    {
+        term: "Care about your craft", description: "Why spend your life developing software unless you care about doing it well?",
+        prompt: "How can you improve your craft?"
+    },
+    {
+        term: "Think! About Your Work", description: "Turn off the autopilot and take control. Constantly critique and appraise your work.",
+        prompt: "How can you critique your work more? How could u integrate it to your pipeline?"
+
+    }
+];
+
+
+module.exports = { pragmatic_programmer };
