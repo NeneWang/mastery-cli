655aa8b3839ad4c8dcc44bb3f73d13f2b199ff92
NelsonWang <wangnelson2@gmail.com>
2023-02-09 18:27:36 -0500

    dsa |  :woman_office_worker:

---
+++ utils/data/dist/dsa.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// TODO: Add all the problems and then define them. 
+var dsa = [];
+var system = [{
+  term: "system design",
+  prompt: "How would you design an Photo App?",
+  description: "",
+  example: "(1) Do you have a real life example? Whats the audience?"
+}];
+module.exports = {
+  system: system
+};
---
+++ utils/data/dsa.js
@@ -0,0 +1,17 @@
+
+
+// TODO: Add all the problems and then define them. 
+const dsa = [
+
+]
+
+const system = [
+    {
+        term: "system design", prompt: "How would you design an Photo App?",
+        description: "",
+        example: "(1) Do you have a real life example? Whats the audience?"
+    }
+]
+
+
+module.exports = {system};
