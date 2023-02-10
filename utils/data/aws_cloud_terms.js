dd9c02d339c63d1bd22bf864bc7bdf8ef0baf66d
NelsonWang <wangnelson2@gmail.com>
2023-02-10 14:31:39 -0500

    features :santa: :woman_office_worker:

--- utils/data/aws_cloud_terms.js
+++ utils/data/aws_cloud_terms.js
@@ -21,7 +21,40 @@ const aws_services = [
         prompt: "Whats the difference between SQS FIFO and Standard queue?",
         term: "sns queues",
         description: "Standard queues provide at-least-once delivery, which means that each message is delivered at least once. FIFO queues provide exactly-once processing, which means that each message is delivered once and remains available until a consumer processes it and deletes it.",
-        
+
+    },
+    {
+        term: "Containers - Dockerization",
+        prompt: "What problem solves for you?",
+        description: "a virtual machine can still sit under-utilized or even idle when the demand is low. If you've ever worked in a role managing a fleet of servers, you would have noticed the dramatic under-utilization of compute during low demand periods. Ideally, we don't want the overhead of deploying and configuring a full operating system every time we need to run a workload.\n\n\
+        To achieve this, we can make use of containerization technologies such as Docker. Docker allows applications running on an operating system to be isolated from each other by running inside a container. Each container holds only the application and associated dependencies (binaries and libraries):",
+    },
+    {
+        term: "Function as a Service (FaaS)",
+        prompt: "What is this and how In what situation do you see this as useful, what real life alternatives can you find?",
+        description: "While containerization technologies such as Docker are commonplace in cloud-native applications, we can take another step further by dividing our applications into discrete functions.\n\n\
+        Function as a Service (FaaS) is another level of abstraction and is where we get to choose the runtime to execute our code on, some execution parameters such as memory allocation, and that's about it. The hardware, operating system, and runtime are all managed for you."
+    },
+    {
+        term: "Scaling Horizontally in AWS",
+        prompt: "What does this mean, and how do you achieve this?",
+        description: "In terms of hardware utilization, the biggest impact here is that we can now scale by workload type. If the user demand was running a task that was creating a high load on the processing tier, we can add more compute nodes (virtual machines) to this tier, independently of the web tier. This is called scaling horizontally",
+    },
+    {
+        term: "EAI",
+        prompt: "What is that and when would you use it?",
+        description: "Enterprise application integration (EAI) refers to the process of connecting different enterprise applications to allow data and functionality to be shared between them. Here are the pros and cons of EAI:\n\
+        Pros:      Increased efficiency: By connecting different enterprise applications, data and functionality can be shared easily between different departments, reducing manual data entry and avoiding duplication of effort.     Improved data accuracy: Integration can ensure that data is consistent and up-to-date across different enterprise applications, reducing the risk of errors and inconsistencies.     \
+        Enhanced collaboration: Integration makes it easier for different departments and teams to work together, as they can access the same information and collaborate on projects in real-time.     Increased competitiveness: Integration can help organizations respond more quickly to changes in the market, allowing them to stay ahead of the competition."
+    },
+    {
+        term: "Event Driven Architecture (EDA)",
+        description: "Event-driven architecture (EDA) is a software design pattern in which the flow of the program is determined by events or messages. In event-driven systems, individual components, or "event emitters," generate events or messages that are then processed by other components, or event handlers.\n\n\
+        Advantages of event-driven architecture include:      Loose coupling: Event-driven systems can allow for a high degree of decoupling between components, which can make it easier to build and maintain large, complex software systems.      Scalability: Event-driven systems can be designed to handle large amounts of data, making them well-suited to cloud computing and big data applications.      Real-time processing: Event-driven systems can process events and update applications in real-time, making them ideal for applications that require immediate responsiveness, such as financial trading systems or social media applications.      Flexibility: Event-driven systems can be highly flexible, as different components can be easily added, removed, or replaced to accommodate changing requirements.",
+        prompt: "How to use this?"
+    },
+    {
+        term: "ESB",
     }
 ]
 
