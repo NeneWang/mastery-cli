3df45d8008d030515dee14e90015546acdcf587c
NelsonWang <wangnelson2@gmail.com>
2023-02-22 22:57:35 -0500

    garbage collect :genie_woman:

--- utils/data/dist/dsa.dev.js
+++ utils/data/dist/dsa.dev.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 /**
  * Introductions to algorithms
  * https://ocw.mit.edu/courses/6-006-introduction-to-algorithms-fall-2011/video_galleries/lecture-videos/
@@ -14,7 +16,11 @@ var system = [{
   prompt: "How would you design an Photo App?",
   description: "",
   example: "(1) Do you have a real life example? Whats the audience?"
-}];
+}, _defineProperty({
+  term: "garbage collection",
+  prompt: "What is this | in which language are they found?",
+  description: "is a dynamic technique for memory management and heap allocation that examines and identifies dead memory blocks before reallocating storage for reuse. Garbage collection's primary goal is to reduce memory leaks."
+}, "prompt", "Garbage collection (GC) is a memory recovery feature built into programming languages such as C# and Java.")];
 module.exports = {
   system: system
 };
