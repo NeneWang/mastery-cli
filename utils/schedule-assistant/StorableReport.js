3757974f23641de6cec6c772ec40f39086032d34
NelsonWang <wangnelson2@gmail.com>
2023-03-07 15:54:33 -0500

    pro: Storable report :crown: :policewoman:

--- utils/schedule-assistant/StorableReport.js
+++ utils/schedule-assistant/StorableReport.js
@@ -1,35 +1,44 @@
 const { JsonDB, Config } = require("node-json-db");
 
+
+const DEBUG = false;
+
+
 class StorableReport {
-  constructor() {
-    this.db = new JsonDB(new Config("report", true, false, "/"));
-    this.report = this.getReport();
-  }
-
-  getDate(){
-    return this.report.date;
-  }
-
-  getReport() {
-    try {
-      return this.db.getData("/");
-    } catch (error) {
-      console.error(error);
-      return {};
+    constructor() {
+        this.db = new JsonDB(new Config("report", true, false, "/"));
+        // this.report = this.getReport();
     }
-  }
 
-  saveReport() {
-    try {
-      this.db.push("/", this.report);
-    } catch (error) {
-      console.error(error);
+    getDate() {
+        // console.log("this.report", this.report);
+        return this.report?.date;
     }
-  }
 
-  setReport(report) {
-    this.report = report;
-  }
+    async getReport() {
+        try {
+            const data = await this.db.getData("/");
+            if (DEBUG) console.log("data", data);
+            this.report = data;
+            return data;
+        } catch (error) {
+            console.error(error);
+            return {};
+        }
+    }
+
+    saveReport() {
+        try {
+            this.db.push("/", this.report);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    setReport(report) {
+        this.report = report;
+        this.saveReport();
+    }
 }
 
 
--- utils/schedule-assistant/tests/test_StorableReport.test.js
+++ utils/schedule-assistant/tests/test_StorableReport.test.js
@@ -1,4 +1,43 @@
-const StorableReport = require('./StorableReport');
+const StorableReport = require('../StorableReport');
+const assert = require('assert');
+
+describe("StorableReport", function () {
+    describe("#getDate()", function () {
+        it("should return the date of the report", async function () {
+            const report = new StorableReport();
+            await report.getReport();
+            const date = report.getDate();
+            console.log("date", date);
+            assert(typeof date === "string");
+        });
+    });
+
+    describe("#setReport(report)", function () {
+        it("should update the report data with the provided report", function () {
+            const report = new StorableReport();
+            const newReport = { date: "2022-01-01", value: 42 };
+            report.setReport(newReport);
+            assert.deepEqual(report.report, newReport);
+        });
+
+        
+        it("should update the report data with the provided report", async function  () {
+            const report = new StorableReport();
+            const newReport = { date: "2022-01-01", value: 10 };
+            report.setReport(newReport);
+
+            // Sets the reports to a new instance and checks if the report is the same.
+            const anotherReport = new StorableReport();
+            await anotherReport.getReport();
+            assert.deepEqual(newReport, anotherReport.report);
+            console.log(newReport, anotherReport.report);
+        });
+    });
+
+    
+
+});
+
 
 
 
