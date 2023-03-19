de0c468957ac7f657538d826612b91134ab8b5fd
NelsonWang <wangnelson2@gmail.com>
2023-03-19 11:28:42 -0400

    Committed by Maid  :zombie_woman:

--- utils/data/interview.js
+++ utils/data/interview.js
@@ -694,24 +694,36 @@ const interview = [
         example: "17. What are the most important things you’d like to see someone accomplish in the first 30, 60, and 90 days on the job? 18. What are the performance expectations of this position over the first 12 months? 19. What is the performance review process like here? How often would I be formally reviewed? 20. What metrics or goals will my performance be evaluated against?"
     },
     {
-        term: "interviewer",
-        prompt: "Asking questions about the interviewer",
+        term: "interviewer-int-questions",
+        prompt: "Ask questions about the interviewer",
         description: "Asking questions of the interviewer shows that you're interested in him or her as a person—and that's a great way to build rapport.",
         example: "21. How long have you been with the company? 22. Has your role changed since you've been here? 23. What did you do before this? 24. Why did you come to this company? 25. What’s your favorite part about working here?"
     },
     {
-        term: "company",
-        prompt: "Asking questions about the company",
+        term: "company-int-questions",
+        prompt: "Ask questions about the company",
         description: "Because you're not just working for one boss or one department, you're working for the company as a whole.",
         example: "26. I've read about the company's founding, but can you tell me more about ___? 27. Where do you see this company in the next few years? 28. What can you tell me about your new products or plans for growth? 29. What are the current goals that the company is focused on, and how does this team work to support hitting those goals? 30. What gets you most excited about the company's future?"
     },
     {
-        term: "team",
-        prompt: "Asking questions about the team",
+        term: "team-int-questions",
+        prompt: "Ask questions about the team",
         description: "The people you work with day in and day out can really make or break your work life. Ask some questions to uncover whether it's the right team for you.",
         example: "31. Can you tell me about the team I’ll be working with? 32. Who will I work with most closely? 33. Who will I report to directly? 34. Can you tell me about my direct reports? What are their strengths and the team's biggest challenges? 35. Do you expect to hire more people in this department in the next six months? 36. Which other departments work most closely with this one? 37. What are the common career paths in this department?"
     },
-    
+    {
+        term: "culture-int-questions",
+        prompt: "Ask questions about the company culture",
+        description: "Is the office buttoned-up conservative or a fly-by-the-seat-of-your-pants kind of place? Learn the subtle, but oh-so-important, aspects of company culture.",
+        example: "38. What is the company and team culture like? 39. How would you describe the work environment here—is the work typically collaborative or more independent? 40. Can you tell me about the last team event you did together? 41. Is there a formal mission statement or company values? (Note: Make sure this isn't Google-able!) 42. What's your favorite office tradition? 43. What do you and the team usually do for lunch? 44. Does anyone on the team hang out outside the office? 45. Do you ever do joint events with other companies or departments? 46. What's different about working here than anywhere else you've worked? 47. How has the company changed since you joined?"
+    },
+    {
+        term: "next-steps-int-questions",
+        prompt: "Ask questions about next steps",
+        description: "Before you leave, make sure the interviewer has all of the information he or she needs and that you're clear on the next steps by asking these questions.",
+        example: "48. Is there anything that concerns you about my background being a fit for this role? 49. What are the next steps in the interview process? 50. Is there anything else I can provide you with that would be helpful? 51. Can I answer any final questions for you?"
+    }
+
 
 
 ]
