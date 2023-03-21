88c0380c8da2c12912124ebf9d4090bfeee92cc5
NelsonWang <wangnelson2@gmail.com>
2023-03-21 14:34:25 -0400

    Machine

--- utils/data/ai_machine.js
+++ utils/data/ai_machine.js
@@ -2,4 +2,14 @@
  * The idea is to have Advanced deep neural networks (Deep learning) and Machine Learning models  and algorithms here.
  */
 
+const pytorch_machine_learning_course = [
+    
+    {
+        term: "cpu-usage-stat-us-hi",
+        prompt: "What does the 'hi' CPU usage statistic represent?",
+        example: "The 'hi' CPU usage statistic represents the percentage of time that the CPU is interrupted by hardware.",
+        description: "In CPU usage statistics, the 'hi' value represents the percentage of time that the CPU is interrupted by hardware, or in other words, the amount of time that the CPU spends handling hardware interrupts. This value is often displayed as part of the output of commands like `top` or `vmstat`." + sample_otutput_top
+    },
+]
+
 
