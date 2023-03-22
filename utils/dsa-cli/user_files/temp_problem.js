d6728849bb4aabdabe428ecf102362c5514d5767
NelsonWang <wangnelson2@gmail.com>
2023-03-22 16:53:26 -0400

    Committed by Maid  :mage_woman:

--- utils/dsa-cli/user_files/temp_problem.js
+++ utils/dsa-cli/user_files/temp_problem.js
@@ -1,38 +1,8 @@
-class ThreeNumberSum {
-    threeSum(array) {
-
-        const res = [];
-        array.sort((a, b) => a - b)
-
-        for (let i = 0; i < array.length; i++) {
-            const a = array[i];
-            if (a > 0) break;
-            if (i > 0 && a === array[i - 1]) continue;
-
-            let l = i + 1;
-            let r = array.length - 1;
-            while (l < r) {
-                const threeSum = a + array[l] + array[r];
-                if (threeSum > 0) {
-                    r--;
-                } else if (threeSum < 0) {
-                    l++;
-                } else {
-                    res.push([a, array[l], array[r]]);
-                    l++;
-                    r--;
-                    while (array[l] === array[l - 1] && l < r) {
-                        l++;
-                    }
-                }
-            }
-        }
-        return res;
-    }
-    solve(array) {
-        return this.threeSum(array);
+class Substraction {
+    solve(a, b) {
+        return 1;
     }
 }
 
 
-module.exports = { Problem: ThreeNumberSum };
+module.exports = { Problem: Substraction };
