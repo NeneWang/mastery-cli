8ecce63564fc61a3fb1999ce159f88a92867556a
NelsonWang <wangnelson2@gmail.com>
2023-03-28 15:28:38 -0400

    Committed by Maid  :elf_woman:

--- utils/data/terms.js
+++ utils/data/terms.js
@@ -13,7 +13,7 @@
 const { TermStorage } = require('../structures');
 
 const set_all = false;
-const study_cisc = true; // Studying for Class Targetted.
+const study_cisc = false; // Studying for Class Targetted.
 
 /**
  * 
@@ -34,7 +34,7 @@ async function populateMasterDeck() {
     const { network, network_midterm, artificialIntelligence, artificialIntelligence_2, algebra, calculousOne } = require("./spring-senior");
     decks.addDeck(new TermStorage(network, "network", { is_active: study_cisc }));
     decks.addDeck(new TermStorage(network_midterm, "network midterm", { is_active: study_cisc }));
-    decks.addDeck(new TermStorage(artificialIntelligence, "artificial intelligence", { is_active: study_cisc }));
+    decks.addDeck(new TermStorage(artificialIntelligence, "artificial intelligence", { is_active: true }));
     decks.addDeck(new TermStorage(artificialIntelligence_2, "artificial intelligence 2", { is_active: study_cisc }));
     decks.addDeck(new TermStorage(algebra, "algebra", { is_active: study_cisc }));
     decks.addDeck(new TermStorage(calculousOne, "calculus one", { is_active: study_cisc }));
