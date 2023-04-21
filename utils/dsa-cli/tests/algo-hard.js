664cebb7b2c8c7e1ec7ba8e3938a6f172141a44f
NelsonWang <wangnelson2@gmail.com>
2023-04-21 00:25:56 -0400

    algo hard | kruskal algorithms :crown: :woman_pilot:

--- utils/dsa-cli/tests/algo-hard.js
+++ utils/dsa-cli/tests/algo-hard.js
@@ -5219,6 +5219,82 @@ class TopologicalSort extends ProblemTests {
 
 }
 
+class KruskalsAlgorithms extends ProblemTests{
+
+    constructor(Problem){
+        super(Problem);
+
+    }
+
+    test_1(){
+        /**
+         * [
+            [
+                [1, 1]
+            ],
+            [
+                [0, 1]
+            ]
+            ]
+            View Outputs Side By Side
+            Input(s)
+            {
+            "edges": [
+                [
+                [1, 1]
+                ],
+                [
+                [0, 1]
+                ]
+            ]
+            }
+         */
+
+        this.current_test_name = '([[1, 1]], [[0, 1]]) => [[1, 1], [0, 1]]';
+        const kruskalsAlgorithms = new this.Problem();
+
+        assert.deepEqual(kruskalsAlgorithms.solve([[1, 1]], [[0, 1]]), [[1, 1], [0, 1]]);
+    }
+
+    test_2(){
+        /**
+         * [
+            [
+                [1, 1]
+            ],
+            [
+                [0, 1],
+                [2, 2]
+            ],
+            [
+                [1, 2]
+            ]
+            ]
+            View Outputs Side By Side
+            Input(s)
+            {
+            "edges": [
+                [
+                [1, 1]
+                ],
+                [
+                [0, 1],
+                [2, 2]
+                ],
+                [
+                [1, 2]
+                ]
+            ]
+            }
+         */
+
+        this.current_test_name = '([[1, 1]], [[0, 1], [2, 2]], [[1, 2]]) => [[1, 1], [0, 1], [2, 2]]';
+        const kruskalsAlgorithms = new this.Problem();
+
+        assert.deepEqual(kruskalsAlgorithms.solve([[1, 1]], [[0, 1], [2, 2]], [[1, 2]]), [[1, 1], [0, 1], [2, 2]]);
+    }
+}
+
 
 
 
