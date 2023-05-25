4bb1d84debd8465dbf4bf08bd22697db84e42e1c
NelsonWang <wangnelson2@gmail.com>
2023-05-25 12:22:51 -0400

    commit :woman_office_worker:

--- utils/utils.js
+++ utils/utils.js
@@ -699,7 +699,7 @@ getArrayLastXDays = (days = 7) => {
 
 increasePerformance = async (feature_name, increaseBY = 1, debug = true) => {
 	try {
-		const res = await axios.post(`${APIDICT.DEPLOYED_MAID}/day_performance/${feature_name}}?increase_score=true&increase_by=${increaseBY}`)
+		const res = await axios.post(`${APIDICT.DEPLOYED_MAID}/day_performance/${feature_name}?increase_score=true&increase_by=${increaseBY}`)
 		if (debug) console.log(res.data);
 	} catch (err){
 		console.warn(err);
