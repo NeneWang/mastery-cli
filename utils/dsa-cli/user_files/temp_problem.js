afedbd5f0ed01c9b2a6c028e1b6d3a65bc092b9e
NelsonWang <wangnelson2@gmail.com>
2023-05-28 00:12:51 -0400

    algo hard changes :herb: :woman_health_worker:

--- utils/dsa-cli/user_files/temp_problem.js
+++ utils/dsa-cli/user_files/temp_problem.js
@@ -1,84 +1,47 @@
-class EvalRPN {
-
-    /**
-     * https://leetcode.com/problems/evaluate-reverse-polish-notation
-     * Time O(N^2) | Space(1)
-     * @param {string[]} tokens
-     * @return {number}
-     */
-    solve(tokens, index = 0) {
-
-        var OPERATORS = {
-            '+': (a, b) => a + b,
-            '-': (a, b) => a - b,
-            '*': (a, b) => a * b,
-            '/': (a, b) => Math.trunc(a / b),
-        };
 
 
-        var performOperation = (tokens, index) => {
-            const [rightNum, leftNum] = [Number(tokens[index - 1]), Number(tokens[index - 2])]
-            const operation = OPERATORS[tokens[index]];
-
-            return operation(leftNum, rightNum);
-        }
-
-        while (1 < tokens.length) {/* Time O(N) */
-            const isOperation = () => tokens[index] in OPERATORS;
-            while (!isOperation()) index++;/* Time O(N) */
-
-            const value = performOperation(tokens, index);
-
-            tokens[index] = value;
-            tokens.splice((index - 2), 2);/* Time O(N) */
-            index--;
-        }
-
-        return tokens[0];
+/** 
+ * https://leetcode.com/problems/design-twitter/
+ * Your Twitter object will be instantiated and called as such:
+ * var obj = new Twitter()
+ * obj.postTweet(userId,tweetId)
+ * var param_2 = obj.getNewsFeed(userId)
+ * obj.follow(followerId,followeeId)
+ * obj.unfollow(followerId,followeeId)
+ */
+class Twitter {
+    constructor() {
+        this.tweets = [];
+        this.following = new Map();
     }
 
+    postTweet(userId, tweetId, { tweets } = this) {
+        tweets.push({ authorId: userId, id: tweetId });
+    }
 
-    /**
-     * https://leetcode.com/problems/evaluate-reverse-polish-notation
-     * Time O(N) | Space(N)
-     * @param {string[]} tokens
-     * @return {number}
-     */
-    solve2(tokens, stack = []) {
-        var OPERATORS = {
-            '+': (a, b) => a + b,
-            '-': (a, b) => a - b,
-            '*': (a, b) => a * b,
-            '/': (a, b) => Math.trunc(a / b),
-        };
+    getNewsFeed(userId, newsIDs = [], { tweets, following } = this) {
+        for (let i = (tweets.length - 1); ((0 <= i) && (newsIDs.length < 10)); i--) {
+            const tweet = tweets[i];
 
-
-        var performOperation = (char, stack) => {
-            const [rightNum, leftNum] = [stack.pop(), stack.pop()];
-            const operation = OPERATORS[char];
-
-            return operation(leftNum, rightNum);
+            const isAuthor = tweet.authorId === userId
+            const isFollowing = following?.get(userId)?.has(tweet.authorId);
+            const canAddTweet = isAuthor || isFollowing
+            if (canAddTweet) newsIDs.push(tweet.id);
         }
 
-        for (const char of tokens) {/* Time O(N) */
-            const isOperation = char in OPERATORS;
-            if (isOperation) {
-                const value = performOperation(char, stack);
+        return newsIDs;
+    }
 
-                stack.push(value);      /* Space O(N) */
+    follow(followerId, followeeId, { following } = this) {
+        if (!following.has(followerId)) following.set(followerId, new Set());
 
-                continue;
-            }
+        following.get(followerId).add(followeeId);
+    }
 
-            stack.push(Number(char));   /* Space O(N) */
-        }
-
-        return stack.pop();
+    unfollow(followerId, followeeId, { following } = this) {
+        if (following.has(followerId)) following.get(followerId).delete(followeeId);
     }
 }
 
 
-
-
-
-module.exports = { Problem: EvalRPN };
+module.exports = { Problem: Twitter };
