68279e82e5b219ad4038fae90ae6834195876d4e
NelsonWang <wangnelson2@gmail.com>
2023-07-29 09:13:41 -0400

    balanced tree

---
+++ utils/dsa-cli/solutions/balanced-binary-tree-c1.js
@@ -0,0 +1,55 @@
+class BalanceBinaryTree {
+
+    isBalanced = function (root) {
+
+        const isChildBalanced = (root) => {
+            const left = this.isBalanced(root.left);
+            const right = this.isBalanced(root.right);
+
+            return left && right
+        }
+
+        const isAcceptableHeight = (root) => {
+            const left = getHeight(root.left);
+            const right = getHeight(root.right);
+
+            const difference = Math.abs(left - right);
+
+            return difference <= 1;
+        }
+
+        const getHeight = (root) => {
+            const isBaseCase = root === null;
+            if (isBaseCase) return 0;
+
+            return dfs(root);
+        }
+
+        var dfs = (root) => {
+            const left = getHeight(root.left)
+            const right = getHeight(root.right);
+
+            const height = Math.max(left, right);
+
+            return height + 1;
+        }
+
+        //TODO  Complete the isBalanced function.
+
+        return true;
+    }
+
+
+    /**
+     * https://leetcode.com/problems/balanced-binary-tree/
+     * TIme O(N) | Space O(H)
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    solve(root) {
+        return this.isBalanced(root);
+    }
+}
+
+
+module.exports = { Problem: BalanceBinaryTree };
--- utils/dsa-cli/user_files/temp_problem.js
+++ utils/dsa-cli/user_files/temp_problem.js
@@ -1,65 +1,9 @@
+class BalanceBinaryTree {
+	solve(root) {
+	// Your code here
 
-class ListNode {
-    constructor(val = 0, next = null) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
-class ReverseKGroup {
-
-
-    /**
-     * https://leetcode.com/problems/reverse-nodes-in-k-group/
-     * Time O(N) | Space O(N)
-     * @param {ListNode} head
-     * @param {number} k
-     * @return {ListNode}
-     */
-    solve(head, k) {
-
-        const moveNode = (curr, k) => {
-            const canMove = () => k && curr;
-            while (canMove()) {
-                curr = curr.next;
-                k--;
-            }
-
-            return [(curr?.next || null), curr];
-        }
-
-        const reverse = ([prev, curr, start]) => {
-            const isSame = () => curr === start;
-            while (!isSame()) {
-                const next = curr.next;
-                curr.next = prev;
-
-                prev = curr;
-                curr = next;
-            }
-        }
-        let tail;
-        const sentinel = tail = new ListNode(0, head);
-
-        while (true) {
-            let [start, last] = moveNode(tail, k);
-            console.log("start", start, "last", last, "tail", tail, "k", k);
-			if (!last) break;
-			
-            reverse([start, tail.next, start])
-
-            const next = tail.next;
-
-            tail.next = last;
-            tail = next;
-
-			console.log("ended with tail:", tail, "tail.next", tail.next, "next", next);
-        }
-
-        return sentinel.next;
-    }
+	}
 }
 
 
-module.exports = { Problem: ReverseKGroup };
-
+module.exports = { Problem: BalanceBinaryTree };
