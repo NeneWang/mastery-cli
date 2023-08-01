d0d642c689aa140def7e12fa263181a4dda028aa
NelsonWang <wangnelson2@gmail.com>
2023-08-01 15:32:23 -0400

    word search ii

--- utils/dsa-cli/base_code/word-search-ii.js
+++ utils/dsa-cli/base_code/word-search-ii.js
@@ -1,8 +1,33 @@
 class FindWords {
-	solve(board, words) {
-	// Your code here
+    solve(board, words) {
+        return new Trie(words).searchBoard(board);
+    };
+}
 
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.word = '';
+    }
+}
+
+class Trie {
+    constructor(words) {
+        this.root = new TrieNode();
+        words.forEach((word) => this.insert(word));
+    }
+
+    /* Time O(N) | Space O(N) */
+    insert(word, node = this.root) {
+        
+    }
+
+    /* Time O((ROWS * COLS) * (4 * (3 ^ (WORDS - 1)))) | Space O(N) */
+    searchBoard(board, node = this.root, words = []) {
+        
 	}
+
+	
 }
 
 
--- utils/dsa-cli/user_files/temp_problem.js
+++ utils/dsa-cli/user_files/temp_problem.js
@@ -1,34 +1,9 @@
+class FindWords {
+	solve(board, words) {
+	// Your code here
 
-class TrieNode {
-    constructor() {
-        this.children = {};
-        this.isWord = false;
-    }
+	}
 }
 
 
-class Trie {
-    constructor() {
-        this.root = new TrieNode();
-    }
-
-    
-	insert(word, node = this.root) {
-        
-    }
-
-    
-	search(word, node = this.root) {
-        
-    }
-
-    
-	startsWith(prefix, node = this.root) {
-    
-        return true;
-    }
-}
-
-
-module.exports = { Problem: Trie };
-
+module.exports = { Problem: FindWords };
