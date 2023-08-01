8677bca3a5047a4fecab315ba82fee86ee1d9df1
NelsonWang <wangnelson2@gmail.com>
2023-08-01 15:15:42 -0400

    implement trie

--- utils/dsa-cli/base_code/implement-trie-prefix-tree.js
+++ utils/dsa-cli/base_code/implement-trie-prefix-tree.js
@@ -1,7 +1,33 @@
-class TrieNode {	// Your code here
 
-	}
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isWord = false;
+    }
 }
 
 
-module.exports = { Problem: TrieNode };
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    
+	insert(word, node = this.root) {
+        
+    }
+
+    
+	search(word, node = this.root) {
+        
+    }
+
+    
+	startsWith(prefix, node = this.root) {
+    
+        return true;
+    }
+}
+
+
+module.exports = { Problem: Trie };
