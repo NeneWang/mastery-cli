709a8810f77f891343dcd0f93b7bd5381a64e121
NelsonWang <wangnelson2@gmail.com>
2023-08-01 13:25:49 -0400

    design add and serach

--- utils/dsa-cli/base_code/design-add-and-search-words-data-structure.js
+++ utils/dsa-cli/base_code/design-add-and-search-words-data-structure.js
@@ -1,7 +1,41 @@
-class TrieNode {	// Your code here
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = new WordDictionary()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
+
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isWord = false;
+    }
+}
+
+
+class WordDictionary {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    addWord(word, node = this.root) {
+        
+    }
+
+    /* Time O(N) | Space O(N) */
+    search(word) {
+
+	}
+
+    dfs(word, node, level) {
+
+	}
+
+    hasWildCard(word, node, level) {
 
 	}
 }
 
 
-module.exports = { Problem: TrieNode };
+module.exports = { Problem: WordDictionary };
--- utils/dsa-cli/user_files/temp_problem.js
+++ utils/dsa-cli/user_files/temp_problem.js
@@ -1,29 +1,7 @@
-class ValidateBinarySearchTree {
-	
+class TrieNode {	// Your code here
 
-	isValidBST(root, min = -Infinity, max = Infinity){
-		const dfs = (root, min, max) => {
-			const left = this.isValidBST(root.left, min, root.val);
-			const right = this.isValidBST(root.right, root.val, max);
-
-			return left && right;
-
-		}
-
-		const isBaseCase = root === null;
-		if(isBaseCase) return true;
-
-		const isInvalid = root.val <= min || root.val >= max;
-		if(isInvalid) return false;
-
-		return dfs(root);
-	}
-
-	solve(root) {
-	// Your code here
-		return this.isValidBST(root);
 	}
 }
 
 
-module.exports = { Problem: ValidateBinarySearchTree };
+module.exports = { Problem: TrieNode };
