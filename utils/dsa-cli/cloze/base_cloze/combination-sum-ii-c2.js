d4f7520f426621b5504c78b184e55ab96944ab7b
NelsonWang <wangnelson2@gmail.com>
2023-08-05 10:41:01 -0400

    combination sum

---
+++ utils/dsa-cli/cloze/base_cloze/combination-sum-ii-c2.js
@@ -0,0 +1,51 @@
+class CombinationSumII {
+
+    /**
+     * https://leetcode.com/problems/combination-sum-ii/
+     * Time O(2^N) | Space O(N)
+     * @param {number[]} candidates
+     * @param {number} target
+     * @return {number[][]}
+     */
+    combinationSum2 = function (candidates, target) {
+
+
+        const dfs = (candidates, target, index = 0, combination = [], combinations = []) => {
+            const isBaseCase = target < 0;
+            if (isBaseCase) return combinations;
+
+            const isTarget = target === 0;
+            if (isTarget) {
+                if (combination.length) combinations.push(combination.slice());
+
+                return combinations
+            }
+
+            for (let i = index; i < candidates.length; i++) {
+                const isDuplicate = (index < i) && (candidates[i - 1] === candidates[i]);
+                if (isDuplicate) continue;
+
+                backTrack(candidates, target, i, combination, combinations);
+            }
+
+            return combinations;
+        }
+
+        const backTrack = (candidates, target, i, combination, combinations) => {
+            combination.push(candidates[i])
+            dfs(candidates, (target - candidates[i]), (i + 1), combination, combinations)
+            combination.pop()
+        }
+
+        candidates.sort((a, b) => a - b)
+
+        return dfs(candidates, target)
+    };
+
+    solve(candidates, target) {
+        return this.combinationSum2(candidates, target);
+    }
+}
+
+
+module.exports = { Problem: CombinationSumII };
--- utils/dsa-cli/user_files/temp_problem.js
+++ utils/dsa-cli/user_files/temp_problem.js
@@ -1,7 +1,46 @@
 class CombinationSumII {
+
+	combinationSum2 = function (candidates, target) {
+	const dfs = (candidates, target, index = 0, combination = [], combinations = []) => {
+
+		const isBaseCase = target < 0;
+		if(isBaseCase) return combinations;
+
+		const isTarget = target === 0;
+		if(isTarget) {
+			if(combination.length) combinations.push(combination.slice());
+
+			return combinations;
+		}
+
+		for(let i = index; i< candidates.length; i++){
+			const isDuplication = (index < i) && (candidates[i - 1] === candidates[i]);
+			if (isDuplication) continue;
+			
+			backtrack(candidates, target, i, combination, combinations);
+
+		}
+
+		return combinations;
+
+	}
+
+	const backtrack = (candidates, target, i, combination, combinations) => {
+		combination.push(candidates[i]);
+		dfs(candidates, (target - candidates[i]), (i + 1), combination, combinations);
+		combination.pop()
+	}
+	
+	candidates.sort((a,b) => a-b)
+		
+	return dfs(candidates, target);
+	
+	};
+
+
 	solve(candidates, target) {
 	// Your code here
-
+		return this.combinationSum2(candidates, target);
 	}
 }
 
