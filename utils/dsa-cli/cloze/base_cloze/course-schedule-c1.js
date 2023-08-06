3b89ae15257ee76115befdcf33ea89ecce91943e
NelsonWang <wangnelson2@gmail.com>
2023-08-06 16:33:37 -0400

    course schedule c1

---
+++ utils/dsa-cli/cloze/base_cloze/course-schedule-c1.js
@@ -0,0 +1,79 @@
+class CourseSchedule {
+
+    /**
+     * https://leetcode.com/problems/course-schedule/
+     * Time O((V)^2 + E) | Space O(V + E)
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish = function (numCourses, prerequisites) {
+
+
+        var initGraph = (numCourses) => ({
+            graph: new Array(numCourses).fill().map(() => []),
+            path: new Array(numCourses).fill(false)
+        })
+
+        var buildGraph = (numCourses, prerequisites) => {
+            const { graph, path } = initGraph(numCourses);
+
+            for (const [src, dst] of prerequisites) {
+                const neighbors = (graph[dst] || []);
+
+                neighbors.push(src);
+
+                graph[dst] = neighbors;
+            }
+
+            return { graph, path };
+        }
+
+        var hasPath = (numCourses, graph, path) => {
+            for (let course = 0; course < numCourses; course++) {
+                if (isCyclic(course, graph, path)) return false;
+            }
+
+            return true;
+        }
+
+        var isCyclic = (currCourse, graph, path) => {
+            const hasSeen = path[currCourse]
+            if (hasSeen) return true
+
+            const isMissingNext = !(currCourse in graph)
+            if (isMissingNext) return false;
+
+            return backTrack(currCourse, graph, path);
+        }
+
+        var backTrack = (currCourse, graph, path) => {
+            path[currCourse] = true;
+            const _hasCycle = hasCycle(currCourse, graph, path)
+            path[currCourse] = false;
+
+            return _hasCycle
+        }
+
+        var hasCycle = (currCourse, graph, path) => {
+            for (const neighbor of graph[currCourse]) {
+                if (isCyclic(neighbor, graph, path)) return true;
+            }
+
+            return false
+        }
+
+        const { graph, path } = buildGraph(numCourses, prerequisites);
+
+        return hasPath(numCourses, graph, path);
+    }
+
+
+
+    solve(numCourses, prerequisistes) {
+        return this.canFinish(numCourses, prerequisistes);
+    }
+}
+
+
+module.exports = { Problem: CourseSchedule };
--- utils/dsa-cli/user_files/temp_problem.js
+++ utils/dsa-cli/user_files/temp_problem.js
@@ -1,54 +1,9 @@
-class Node {
-	constructor(val) {
-		this.val = val;
-		this.neighbors = [];
-	}
+class CourseSchedule {
+	solve(numCourses, prerequisistes) {
+	// Your code here
 
-	push(neighbor) {
-		this.neighbors.push(neighbor);
-	}
-
-}
-
-
-class CloneGraph {
-
-
-	/**
-	 * https://leetcode.com/problems/clone-graph/
-	 * Time O(V + E) | Space O(N)
-	 * @param {Node} node
-	 * @return {Node}
-	 */
-	cloneGraph = function (node, seen = new Map()) {
-		
-		const dfs = (node, seen) => {
-
-			const clone = new Node(node.val);
-			seen.set(node, clone);
-
-			for (const neighbor of node.neighbors){
-				const cloneNeighbor = this.cloneGraph(neighbor, seen);
-				clone.neighbors.push(cloneNeighbor);
-			}
-
-			return clone;
-
-		}
-		
-		const isBaseCase = node === null;
-		if(isBaseCase) return null;
-
-		if(seen.has(node)) return seen.get(node);
-		return dfs(node, seen);
-
-
-	}
-
-	solve(node) {
-		return this.cloneGraph(node);
 	}
 }
 
 
-module.exports = { Problem: CloneGraph };
+module.exports = { Problem: CourseSchedule };
