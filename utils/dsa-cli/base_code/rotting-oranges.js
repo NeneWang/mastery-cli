425d86142e57a576d5cb023201b793aefd66016e
NelsonWang <wangnelson2@gmail.com>
2023-08-09 16:20:08 -0400

    rotting organges

--- utils/dsa-cli/base_code/rotting-oranges.js
+++ utils/dsa-cli/base_code/rotting-oranges.js
@@ -1,9 +1,51 @@
-class Queue {
-	solve(grid) {
-	// Your code here
 
-	}
+class Queue {
+
+    constructor() {
+        this.queue = []
+    }
+
+    enqueue(element) {
+        this.queue.push(element)
+    }
+
+    dequeue() {
+        return this.queue.shift()
+    }
+
+    front() {
+        return this.queue[0]
+    }
+
+    size() {
+        return this.queue.length
+    }
+
+    isEmpty() {
+        return this.size() === 0
+    }
+
 }
 
 
-module.exports = { Problem: Queue };
+class RottingOranges {
+
+
+    /**
+     * https://leetcode.com/problems/rotting-oranges/
+     * Time O(ROWS * COLS) | Space O(ROWS * COLS)
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    orangesRotting = function (grid) {
+		
+    };
+
+
+    solve(grid) {
+        return this.orangesRotting(grid);
+    }
+}
+
+
+module.exports = { Problem: RottingOranges };
--- utils/dsa-cli/user_files/temp_problem.js
+++ utils/dsa-cli/user_files/temp_problem.js
@@ -1,57 +1,9 @@
-class ReduntantConnection {
+class Queue {
+	solve(grid) {
+	// Your code here
 
-
-    /**
-     * https://leetcode.com/problems/redundant-connection/
-     * Time O((V)^2 + E) | Space O(V + E)
-     * @param {number[][]} edges
-     * @return {number[]}
-     */
-    findRedundantConnection = function (edges) {
-
-
-        const hasRedundantConnection = (graph, source, target, seen = new Set()) => {
-            if (seen.has(source)) return false
-            seen.add(source);
-
-            const isEqual = source === target
-            if (isEqual) return true;
-
-            return dfs(graph, source, target, seen);
-        }
-
-        const dfs = (graph, source, target, seen) => {
-            for (const neighbor of graph[source]) {
-                if (hasRedundantConnection(graph, neighbor, target, seen)) return true;
-            }
-
-            return false;
-        }
-
-
-        const graph = new Array((1000 + 1)).fill().map(() => []);
-
-        // TODO Iterate over each of the edges, and check if there is a redundant connection.
-        // TODO There are nodes in the graph wheather src is in graph or destiny. Add them both after iterating.
-        // TODO If there is nodes and there is a redundant connection return the src and dst.
-		
-		for(const [src, dst] of edges){
-			
-			const hasNode = (src in graph) && (dst in graph);
-			if(hasNode && hasRedundantConnection(graph, src, dst)) return [src, dst];
-
-			graph[src].push(dst);
-			graph[dst].push(src);
-
-		}
-
-
-    }
-
-    solve(edges) {
-        return this.findRedundantConnection(edges);
-    }
+	}
 }
 
 
-module.exports = { Problem: ReduntantConnection };
+module.exports = { Problem: Queue };
