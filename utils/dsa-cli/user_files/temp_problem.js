f1ebc7d6c520a4ac363a2664d747ceb7ac78ad69
NelsonWang <wangnelson2@gmail.com>
2023-08-24 16:02:08 -0400

    multiply strings

--- utils/dsa-cli/user_files/temp_problem.js
+++ utils/dsa-cli/user_files/temp_problem.js
@@ -1,36 +1,9 @@
+class MultiplyStrings {
+	solve(num1, num2) {
+	// Your code here
 
-
-var isOpened = (char) => char === '('
-var isClosed = (char) => char === ')'
-
-class CheckValidString {
-
-    /**
-     * Time O(N) | Space O(1)
-     * @param {string} s
-     * @return {boolean}
-     */
-    checkValidString = function (s) {
-        let [left, right] = [0, 0];
-
-        for (const char of s) {/* Time O(N) */
-            left += isOpened(char) ? 1 : -1;
-            right += !isClosed(char) ? 1 : -1;
-
-            const isNegative = right < 0;
-            if (isNegative) break;
-
-            left = Math.max(left, 0);
-        }
-
-        return left === 0;
-    }
-
-    solve(s) {
-        return this.checkValidString(s);
-    }
+	}
 }
 
 
-module.exports = { Problem: CheckValidString };
-
+module.exports = { Problem: MultiplyStrings };
