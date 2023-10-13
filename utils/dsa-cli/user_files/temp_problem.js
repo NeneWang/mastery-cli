49e0c09401af8e695d0bb384f5f29987a2c61e36
NelsonWang <wangnelson2@gmail.com>
2023-10-13 15:24:54 -0400

    utils functions

--- utils/dsa-cli/user_files/temp_problem.js
+++ utils/dsa-cli/user_files/temp_problem.js
@@ -1,47 +1,9 @@
+class Candy {
+	solve(ratings) {
+	// Your code here
 
-
-/** 
- * https://leetcode.com/problems/design-twitter/
- * Your Twitter object will be instantiated and called as such:
- * var obj = new Twitter()
- * obj.postTweet(userId,tweetId)
- * var param_2 = obj.getNewsFeed(userId)
- * obj.follow(followerId,followeeId)
- * obj.unfollow(followerId,followeeId)
- */
-class Twitter {
-    constructor() {
-        this.tweets = [];
-        this.following = new Map();
-    }
-
-    postTweet(userId, tweetId, { tweets } = this) {
-        // TODO Complete the postTweet function which just pushes the tweet to the tweets array.
-    }
-
-    getNewsFeed(userId, newsIDs = [], { tweets, following } = this) {
-        for (let i = (tweets.length - 1); ((0 <= i) && (newsIDs.length < 10)); i--) {
-            const tweet = tweets[i];
-
-            const isAuthor = tweet.authorId === userId
-            const isFollowing = following?.get(userId)?.has(tweet.authorId);
-            const canAddTweet = isAuthor || isFollowing
-            if (canAddTweet) newsIDs.push(tweet.id);
-        }
-
-        return newsIDs;
-    }
-
-    follow(followerId, followeeId, { following } = this) {
-        if (!following.has(followerId)) following.set(followerId, new Set());
-
-        following.get(followerId).add(followeeId);
-    }
-
-    unfollow(followerId, followeeId, { following } = this) {
-        if (following.has(followerId)) following.get(followerId).delete(followeeId);
-    }
+	}
 }
 
 
-module.exports = { Problem: Twitter };
+module.exports = { Problem: Candy };
