782aa6aafe756c1f5f645b44131459b00b852461
NelsonWang <wangnelson2@gmail.com>
2023-10-18 15:53:43 -0400

    readme update ::

--- utils/dsa-cli/user_files/temp_problem.js
+++ utils/dsa-cli/user_files/temp_problem.js
@@ -1,9 +1,45 @@
-class Candy {
-	solve(ratings) {
-	// Your code here
+class RotateImage {
 
-	}
+    /**
+     * Time O(ROWS * COLS) | Space O(1)
+     * https://leetcode.com/problems/rotate-image/
+     * @param {number[][]} matrix
+     * @return {void} Do not return anything, modify matrix in-place instead.
+     */
+    rotate = (matrix) => {
+
+
+
+        var transpose = (matrix) => {
+            // TODO Implement the transpose wheras you can think of it as fliping by the first rows first element and the last row last element as the pivots.
+
+        };
+
+        var swap1 = (matrix, row, col) => [matrix[row][col], matrix[col][row]] = [matrix[col][row], matrix[row][col]];
+
+        var reflect = (matrix) => {
+            const rows = matrix.length;
+
+            for (let row = 0; (row < rows); row++) {/* Time O(ROWS) */
+                for (let col = 0; (col < (rows / 2)); col++) {/* Time O(COLS) */
+                    const reflection = ((rows - col) - 1);
+
+                    swap2(matrix, row, col, reflection);
+                }
+            }
+        }
+
+        var swap2 = (matrix, row, col, reflection) => [matrix[row][col], matrix[row][reflection]] = [matrix[row][reflection], matrix[row][col]];
+
+        transpose(matrix);/* Time O(ROWS * COLS) */
+        reflect(matrix);  /* Time O(ROWS * COLS) */
+    };
+
+
+    solve(matrix) {
+        return this.rotate(matrix);
+    }
 }
 
 
-module.exports = { Problem: Candy };le.exports = { Problem: SubstringWithConcatenationOfAllWords };
+module.exports = { Problem: RotateImage };
