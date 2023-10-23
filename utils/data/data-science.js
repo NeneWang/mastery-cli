41df69b4dee83e617fff1aefcd7490c28701dd51
NelsonWang <wangnelson2@gmail.com>
2023-10-23 17:25:57 -0400

    adding data science related problems ::

--- utils/data/data-science.js
+++ utils/data/data-science.js
@@ -44,9 +44,77 @@ const statistics = [
     },
     {
         term: "accuracy",
-        prompt: "Use example of accuracy",
-        description: "Accuracy is the relationship between the measuremen and the actual truth",
-        
+        prompt: "Use accuracy correctly in a setence, or what is it useful for?",
+        description: "Accuracy is the relationship between the measurement and the actual truth (Inversely related to bias)",
+        example: "The more accurate your model is. The closest it is with providing the truth without any bias"
+    },
+    {
+        term: "Precision",
+        prompt: "Use precision correctly in a setence, or what is it useful for?",
+        description: "Precision is certaintity of each measurement (Inversely related to variance)",
+        example: "Precision is low if it changes everytime I re-measure it." 
+    },
+    {
+        term: "Resolution",
+        prompt: "Use resolution correctly in a setence, or what is it useful for?",
+        description: "The number of data points per unit measurement (time, space, individual, etc.)",
+        example: "I am measuring at 100hz which means that I have 100 data points per second of resolution"
+    },
+    {
+        term: "Gaussian distribution",
+        prompt: "What does a Gaussian distribution suggest?",
+        description: "showing that data near the mean are more frequent in occurrence than data far from the mean.",
+        example: "Suggests that the data is normally distributed having a bell curve in the middle"
+    },
+    {
+        term: "T distribution",
+        prompt: "What does a T distribution suggest?",
+        description: "A T distribution is similar to a Gaussian distribution but has fatter tails",
+        example: "Suggests that the data is normally distributed having a bell curve in the middle"
+    },
+    {
+        term: "unimodal and bimodal",
+        prompt: "What does a unimodal and bimodal distribution suggest?",
+        description: "A unimodal distribution has one peak, a bimodal distribution has two peaks",
+    },
+    {
+        term: "Plot a Gaussian distribution",
+        prompt: "Plot a Gaussian distribution from -4 to 4 having 1001 discretizations",
+        description: "# import libraries\n\
+        import matplotlib.pyplot as plt\n\
+        import numpy as np\n\
+        import scipy.stats as stats",
+        example: "x = np.linspace(-4, 4, 1001)\n\
+        y = stats.norm.pdf(x, 0, 1)\n\
+        plt.plot(x, y)"
+    },
+    {
+        term: "Plot a Normally distributed Histogram ",
+        prompt: "Plot a Normally distributed Histogram (25 bins) of random data using 1000 points using randn shifted by 5",
+        description: "# import libraries\n\
+        import matplotlib.pyplot as plt\n\
+        import numpy as np\n\
+        import scipy.stats as stats",
+        example: "data = np.random.randn(1000) + 5\n\
+        plt.hist(data, bins=25)\n\
+        plt.show()"
+    },
+    {
+        term: "Calculate the variance",
+        prompt: "Calculate the variance of [ 8, 0, 4, 1, -2, 7] knowing that the mean is 3",
+        description: "Variance is the average (divide by n - 1) of the squared differences from the Mean",
+        example: "8 - 3 = 5\n\
+        0 - 3 = -3\n\
+        4 - 3 = 1\n\
+        1 - 3 = -2\n\
+        -2 - 3 = -5\n\
+        7 - 3 = 4\n\
+        (5^2 + (-3)^2 + 1^2 + (-2)^2 + (-5)^2 + 4^2) / 5 = 16"
+    },
+    {
+        term: "Larger variance",
+        prompt: "What does a larger variance mean?",
+        example: "A larger variance means that the data is more spread out",
     }
 
 ]
--- utils/dsa-cli/user_files/temp_problem.js
+++ utils/dsa-cli/user_files/temp_problem.js
@@ -1,248 +1,78 @@
-class Candy {
-	solve(ratings) {
-	// Your code here
-
-	}
-}
 
 
-module.exports = { Problem: Candy };le.exports = { Problem: SubstringWithConcatenationOfAllWords };romEnd };m: AverageOfLevels };nimumDifference };
-module.exports = { Problem: PartitionList };
-	}
-}
+var setCellsToZero = (matrix) => {
+    const [rows, cols] = [matrix.length, matrix[0].length];
 
+    for (let row = 1; (row < rows); row++) {/* Time O(ROWS) */
+        for (let col = 1; (col < cols); col++) {/* Time O(COLS) */
+            const isZero = ((matrix[row][0] === 0) || (matrix[0][col] == 0));
+            if (!isZero) continue;
 
-module.exports = { Problem: IsSymmetric };
-    isEmpty() {
-        return this.size() === 0
-    }
-
-}
-
-class WordLadder {
-
-
-    /**
-     * https://leetcode.com/problems/word-ladder/
-     * Time O(ROWS * COLS) | Space O(ROWS * COLS)
-     * @param {string} beginWord
-     * @param {string} endWord
-     * @param {string[]} wordList
-     * @return {number}
-     */
-    ladderLength = function (beginWord, endWord, wordList) {
-		
-	};
-
-
-    solve(beingWord, endWord, wordList) {
-        return this.ladderLength(beingWord, endWord, wordList);
+            matrix[row][col] = 0;
+        }
     }
 }
 
+var setEdgesToZero = (matrix, isColZero = false) => {
+    const [rows, cols] = [matrix.length, matrix[0].length];
 
-module.exports = { Problem: WordLadder }; index + 2
-			let leftChild, rightChild
-			let swap = null
+    for (let row = 0; (row < rows); row++) {/* Time O(ROWS) */
+        if (matrix[row][0] === 0) isColZero = true;
 
-			if (leftChildIndex < length) {
-				leftChild = this.heap[leftChildIndex]
-				if (leftChild > element) {
-					swap = leftChildIndex
-				}
-			}
+        for (let col = 1; (col < cols); col++) {/* Time O(COLS) */
+            const canSet = (matrix[row][col] === 0);
+            if (!canSet) continue;
 
-			if (rightChildIndex < length) {
-				rightChild = this.heap[rightChildIndex]
-				if (
-					(swap === null && rightChild > element) ||
-					(swap !== null && rightChild > leftChild)
-				) {
-					swap = rightChildIndex
-				}
-			}
-
-			if (swap === null) break
-			this.swap(index, swap)
-			index = swap
-		}
-	}
-
-	front() {
-		return this.heap[0]
-	}
-
-	size() {
-		return this.heap.length
-	}
-
-	isEmpty() {
-		return this.size() === 0
-	}
-
-}
-
-
-class LastStoneWeight {
-	/**
-	 * https://leetcode.com/problems/last-stone-weight/
-	 * Time O(N * log(N)) | Space O(N)
-	 * @param {number[]} stones
-	 * @return {number}
-	 */
-	lastStoneWeight = (stones) => {
-
-	};
-
-
-
-	solve(stones) {
-		return this.lastStoneWeight(stones);
-	}
-}
-
-
-module.exports = { Problem: LastStoneWeight };
-   front() {
-        return this.heap[0]
-    }
-
-    size() {
-        return this.heap.length
-    }
-
-    isEmpty() {
-        return this.size() === 0
-    }
-
-}
-
-class KthLargestElementInAnArray {
-	solve(nums, k) {
-	// Your code here
-
-	}
-}
-
-
-module.exports = { Problem: KthLargestElementInAnArray };ment) {
-        this.heap.push(element)
-        this.bubbleUp()
-    }
-
-    bubbleUp() {
-        let index = this.heap.length - 1
-        while (index > 0) {
-            const parentIndex = Math.floor((index - 1) / 2)
-            if (this.heap[parentIndex] >= this.heap[index]) break
-            this.swap(parentIndex, index)
-            index = parentIndex
+            matrix[0][col] = 0;
+            matrix[row][0] = 0;
         }
     }
 
-    swap(index1, index2) {
-        const temp = this.heap[index1]
-        this.heap[index1] = this.heap[index2]
-        this.heap[index2] = temp
-    }
-
-    dequeue() {
-        const max = this.heap[0]
-        const end = this.heap.pop()
-        if (this.heap.length > 0) {
-            this.heap[0] = end
-            this.sinkDown()
-        }
-        return max
-    }
-
-    sinkDown() {
-        let index = 0
-        const length = this.heap.length
-        const element = this.heap[0]
-        while (true) {
-            const leftChildIndex = 2 * index + 1
-            const rightChildIndex = 2 * index + 2
-            let leftChild, rightChild
-            let swap = null
-
-            if (leftChildIndex < length) {
-                leftChild = this.heap[leftChildIndex]
-                if (leftChild > element) {
-                    swap = leftChildIndex
-                }
-            }
-
-            if (rightChildIndex < length) {
-                rightChild = this.heap[rightChildIndex]
-                if (
-                    (swap === null && rightChild > element) ||
-                    (swap !== null && rightChild > leftChild)
-                ) {
-                    swap = rightChildIndex
-                }
-            }
-
-            if (swap === null) break
-            this.swap(index, swap)
-            index = swap
-        }
-    }
-
-    front() {
-        return this.heap[0]
-    }
-
-    size() {
-        return this.heap.length
-    }
-
-    isEmpty() {
-        return this.size() === 0
-    }
-
-    top(){
-        return this.heap[0]
-    }
-
-
+    return isColZero;
 }
 
+var setFirstRowZero = (matrix, cols = matrix[0].length) => {
+    for (let col = 0; (col < cols); col++) {/* Time O(COLS) */
+        matrix[0][col] = 0;
+    }
+}
 
-/** 
- * https://leetcode.com/problems/find-median-from-data-stream/
- * Your MedianFinder object will be instantiated and called as such:
- * var obj = new MedianFinder()
- * obj.addNum(num)
- * var param_2 = obj.findMedian()
+var setFirstColZero = (matrix, rows = matrix.length) => {
+    // TODO For each row, set the first element to zero
+    for (let row = 0; (row < rows); row++) {/* Time O(ROWS) */
+        matrix[row][0] = 0;
+    }
+}
+
+/**
+ * Constant Space
+ * Time O(ROWS * COLS) | Space (1)
+ * https://leetcode.com/problems/set-matrix-zeroes/
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
  */
-class MedianFinder {
-    constructor() {
-        this.maxHeap = new MaxPriorityQueue()
-        this.minHeap = new MinPriorityQueue()
-    }
+const setZeroes = (matrix) => {
 
-    /* Time O(log(N)) | Space (N) */
-    insertNum(num) {
-    
-	}
 
-    addNum(num, heap = this.getHeap(num)) {
-     
-	}
+    const isColZero = setEdgesToZero(matrix);/* Time O(ROWS * COLS) */
 
-    getHeap(num, { maxHeap, minHeap } = this) {
-        
-    }
+    setCellsToZero(matrix);                  /* Time O(ROWS * COLS) */
 
-    rebalance({ maxHeap, minHeap } = this) {
-    }
+    const isZero = (matrix[0][0] === 0);
+    if (isZero) setFirstRowZero(matrix);     /* Time O(COLS) */
 
-    /* Time O(1) | Space (1) */
-    findMedian({ maxHeap, minHeap } = this) {
-    }
-
+    if (isColZero) setFirstColZero(matrix);  /* Time O(ROWS) */
 }
 
 
-module.exports = { Problem: MedianFinder };
+class SetMatrixZeroes {
+
+
+
+    solve(matrix) {
+        return setZeroes(matrix);
+    }
+}
+
+
+module.exports = { Problem: SetMatrixZeroes };
