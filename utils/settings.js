14e6349a07edf5c01ed874829d8393c3e4f0a957
NelsonWang <wangnelson2@gmail.com>
2024-01-12 12:26:52 -0500

    Commited by Maid ::

--- utils/settings.js
+++ utils/settings.js
@@ -1,46 +1,41 @@
 module.exports = Settings = {
-    "username": "Nelson",
     "account_id": 1,
-    "disable_maid_commit_cateogory": true,
-    "disable_maid_all": false,
+    "ask_quiz_when_commit": true,
+    "quiz_enabled": [
+        "math",
+        "term",
+    ], //Quick quizes enabled when commiting, 
     "commit_categories": [
         {
             "code": "feat",
+            "description": "Personal Projects Feature Label",
             "icon_list": [
                 ":hammer:",
                 ":shipit:",
                 ":ambulance:"
             ],
             "feature_name": "feat"
+        },
+        {
+            "code": "pro",
+            "description": "Professional Projects Feature Label",
+            "icon_list": [],
+            "feature_name": "pro"
         }
-    ],
-    "maid_name": "Lily",
-    "maid_icons": [
-        ":hammer:",
     ],
     "report_settings": {
         "week_features": true,
         "performance_summary": true,
         "weather": false,
-        "missing_report": true
-    }, //Order matters, "quiz_enabled"
-    "quiz_enable": [
-        "math"
-    ], //Quick quizes enabled when commiting, 
-    "quiz_allow_reattempts": 3,
-    "terms_force_mode_cards" : 2, //Cards fetched when in force mode.
-    "strict_commit": false,
-    "features_ignore": [
-        "features",
-        "boba_tea_drank"
-    ], //Fetures to ignore on the report.
-    "monday_ses": [
-        ""
-    ],
-    "table_feat_show": ["commits", "feat", "algo_w", "pro", "math_ss", "acad"], // commits │ feat │ math_ss │ ref │ fix  │ algo │ acad │ pro  │ terms │ algo_w │ test │ tesrasd │ commits} │ terms}
+        "missing_report": true,
+        "Objectives Journal": true,
+    },
+    "terms_force_mode_cards": 3, //Cards fetched when in force mode.
+    "long_term_storage_of_fail_terms": true, // If a user commits a mistake in a term, it will be stored to be used on next quiz.
+    "table_feat_show": ["commits", "feat", "algo_w", "pro", "math_ss"], // commits │ feat │ math_ss │ ref │ fix  │ algo │ acad │ pro  │ terms │ algo_w │ test │ tesrasd │ commits} │ terms}
     "show_http_errors": false,
     "dev_mode": false, //If true, it will print communication receips
     "account_settings": {
-    "access_credentials_enabled": true,
+        "access_credentials_enabled": true,
     }
 }
