33b2b7694727780998ad4c742f31084bc1d6a480
NelsonWang <wangnelson2@gmail.com>
2024-03-01 17:33:49 -0500

    save local ::

--- utils/settings.js
+++ utils/settings.js
@@ -1,5 +1,16 @@
 // Import from settings.json from /data/settings.json
-const { getDirAbsoluteUri } = require('./utils_functions');
+/**
+ * Gets directory path
+ * @param {str} fileimage : String containing the relative position of the image from utils directory
+ * @returns {str} Formatted C:/github/testing/mastery-cli/img/unicorn.png
+ */
+const getDirAbsoluteUri = (fileimage = './img/unicorn.png', subdirectory = './data/') => {
+    // Note it should take from the root.
+    const absolutePath = path.resolve(path.join(__dirname, './data/', fileimage)); // Note the '../' because it is inside of constants
+
+    // const fileUrl = url.pathToFileURL(abosolutePath);
+    return (absolutePath.toString());
+};
 
 const absolute_settings_uri = getDirAbsoluteUri(`data/settings.json`);
 let settings = {};
