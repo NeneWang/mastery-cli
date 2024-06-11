d5d4747b8964aa510a8e24e040abe4cd35ee0262
NelsonWang <wangnelson2@gmail.com>
2024-06-11 03:42:41 -0400

    WIP ::

--- utils/data/terms.js
+++ utils/data/terms.js
@@ -46,7 +46,6 @@ const longTermCareer = new DeckMask(
         decksToEnableStrings: [
             "discrete_math",
             "probability",
-            "analysisAlgorithmClass",
         
         ]
 
